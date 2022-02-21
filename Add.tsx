ba6e719d75637f51539219c27c38e71515bbed5e
zach taylor <taylor67469@yahoo.com>
2022-02-21 14:21:09 -0600

    working on the buttons!

--- Add.tsx
+++ Add.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
 export default function Add(){
     const [add,setAdd]=useState(0);
+    const [number, onChangeNumber] = useState();
     return(
+        <div>
+        <TextInput
+        style={styles.input}
+        value={number}
+        placeholder="Add a budget"
+        keyboardType="numeric"
+      />
     <Pressable style={styles.myButtons} onPress={()=>setAdd(add+1)}>
         <Text style={styles.myText}>Add budget</Text>
     </Pressable>
+    </div>
     );
 
 }
@@ -15,5 +24,11 @@ const styles = StyleSheet.create({
       },
       myText:{
         color:'black',
+      },
+      input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
       },
   });
--- Sub.tsx
+++ Sub.tsx
@@ -1,10 +1,21 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
 export default function Sub(){
     const [sub,setSub]=useState(0);
-    return(<Pressable style={styles.myButtons} onPress={()=>setSub(sub+1)}>
-        <Text style={styles.myText}>Add expense</Text>
-    </Pressable>);
+    const [number, onChangeNumber] = useState();
+    return(
+    <div>
+        <TextInput
+        style={styles.input}
+        value={number}
+        placeholder="Subtract an Expense"
+        keyboardType="numeric"
+      />
+    <Pressable style={styles.myButtons} onPress={()=>setSub(sub+1)}>
+        <Text style={styles.myText}>Subtract an expense</Text>
+    </Pressable>
+    </div>
+    );
 
 }
 const styles = StyleSheet.create({
@@ -13,5 +24,12 @@ const styles = StyleSheet.create({
       },
       myText:{
         color:'black',
+        marginLeft:0,
+      },
+      input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
       },
   });
